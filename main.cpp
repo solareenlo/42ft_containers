f7b6b4e7637b1aeead35a053f0136a96ca88cc2e
solareenlo <solareenlo@gmail.com>
2021-11-30 11:29:48 +0900

    Update: main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 05:35:30 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/11/30 06:19:50 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/11/30 11:25:08 by tayamamo         ###   ########.fr       */
 /*   Copyright 2021                                                           */
 /* ************************************************************************** */
 
@@ -22,5 +22,9 @@ int main() {
     for (int i = 0; i < 5; i++) {
         std::cout << "v[" << i << "]: " << v[i] << std::endl;
     }
+    v.resize(10);
+    std::cout << "size: " << v.size() << std::endl;
+    v.resize(0);
+    std::cout << "size: " << v.size() << std::endl;
     return 0;
 }
