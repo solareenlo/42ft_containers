e29e66c3e17274d5f77ac5b966395f8dfaa9c07b
solareenlo <solareenlo@gmail.com>
2021-12-02 06:33:28 +0900

    Update: main.cpp

--- main.cpp
+++ main.cpp
@@ -6,11 +6,12 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 05:35:30 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/12/02 05:15:44 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/12/02 06:32:42 by tayamamo         ###   ########.fr       */
 /*   Copyright 2021                                                           */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <map>
 #include <string>
 #include <typeinfo>
 
@@ -76,6 +77,124 @@ void testPairConstructor() {
               << '\n';
 }
 
+void testReverseIteratorOperatorMinusNonmember() {
+    ft::vector<int> v;
+    for (int i = 0; i < 10; i++) v.push_back(i);
+    ft::reverse_iterator<ft::vector<int>::iterator> from, until;
+    from = v.rbegin();
+    until = v.rend();
+    std::cout << "v has " << (until - from) << " elements.\n";
+}
+
+void testReverseIteratorOperatorPlusNonmember() {
+    ft::vector<int> v;
+    for (int i = 0; i < 10; i++) v.push_back(i);
+    typedef ft::vector<int>::iterator iter_type;
+    ft::reverse_iterator<iter_type>   rev_it;
+    rev_it = 3 + v.rbegin();
+    std::cout << "The fourth element from the end is: " << *rev_it << '\n';
+}
+
+void testReverseIteratorRelationalOperators() {
+    ft::vector<int> v;
+    for (int i = 0; i < 10; i++) v.push_back(i);
+    typedef ft::vector<int>::iterator iter_type;
+    iter_type                         from(v.begin());
+    iter_type                         until(v.end());
+    ft::reverse_iterator<iter_type>   rev_until(from);
+    ft::reverse_iterator<iter_type>   rev_from(until);
+    std::cout << "v:";
+    while (rev_from != rev_until) std::cout << ' ' << *rev_from++;
+    std::cout << '\n';
+}
+
+void testReverseIteratorOperatorOffset() {
+    ft::vector<int> v;
+    for (int i = 0; i < 10; i++) v.push_back(i);
+    typedef ft::vector<int>::iterator iter_type;
+    ft::reverse_iterator<iter_type>   rev_iterator = v.rbegin();
+    std::cout << "The fourth element from the end is: " << rev_iterator[3]
+              << '\n';
+}
+
+void testReverseIteratorOperatorPointer() {
+    // std::map<int, std::string> numbers;
+    // numbers.insert(std::make_pair(1, "one"));
+    // numbers.insert(std::make_pair(2, "two"));
+    // numbers.insert(std::make_pair(3, "three"));
+    // typedef std::map<int, std::string>::iterator map_iter;
+    // ft::reverse_iterator<map_iter>              rev_end(numbers.begin());
+    // ft::reverse_iterator<map_iter>              rev_iterator(numbers.end());
+    // for (; rev_iterator != rev_end; ++rev_iterator)
+    //     std::cout << rev_iterator->first << ' ' << rev_iterator->second <<
+    //     '\n';
+}
+
+void testReverseIteratorOperatorMinusEqual() {
+    ft::vector<int> v;
+    for (int i = 0; i < 10; i++) v.push_back(i);
+    typedef ft::vector<int>::iterator iter_type;
+    ft::reverse_iterator<iter_type>   rev_iterator = v.rend();
+    rev_iterator -= 4;
+    std::cout << "rev_iterator now points to: " << *rev_iterator << '\n';
+}
+
+void testReverseIteratorOperatorMinusMinus() {
+    ft::vector<int> v;
+    for (int i = 0; i < 10; i++) v.push_back(i);
+    typedef ft::vector<int>::iterator iter_type;
+    ft::reverse_iterator<iter_type>   rev_begin(v.end());
+    ft::reverse_iterator<iter_type>   rev_end(v.begin());
+    ft::reverse_iterator<iter_type>   rev_iterator = rev_begin;
+    while (rev_iterator != rev_end) std::cout << *rev_iterator++ << ' ';
+    std::cout << '\n';
+    while (rev_iterator != rev_begin) std::cout << *(--rev_iterator) << ' ';
+    std::cout << '\n';
+}
+
+void testReverseIteratorOperatorMinus() {
+    ft::vector<int> v;
+    for (int i = 0; i < 10; i++) v.push_back(i);
+    typedef ft::vector<int>::iterator iter_type;
+    ft::reverse_iterator<iter_type>   rev_iterator;
+    rev_iterator = v.rend() - 3;
+    std::cout << "v.rend()-3 points to: " << *rev_iterator << '\n';
+}
+
+void testReverseIteratorOperatorPlusEqual() {
+    ft::vector<int> v;
+    for (int i = 0; i < 10; i++) v.push_back(i);
+    typedef ft::vector<int>::iterator iter_type;
+    ft::reverse_iterator<iter_type>   rev_iterator = v.rbegin();
+    rev_iterator += 2;
+    std::cout << "The third element from the end is: " << *rev_iterator << '\n';
+}
+
+void testReverseIteratorOperatorPlusPlus() {
+    ft::vector<int> v;
+    for (int i = 0; i < 10; i++) v.push_back(i);
+    typedef ft::vector<int>::iterator iter_type;
+    iter_type                         from(v.begin());
+    iter_type                         until(v.end());
+    ft::reverse_iterator<iter_type>   rev_until(from);
+    ft::reverse_iterator<iter_type>   rev_from(until);
+    std::cout << "v:";
+    while (rev_from != rev_until) {
+        std::cout << ' ' << *rev_from;
+        ++rev_from;
+    }
+    std::cout << '\n';
+}
+
+void testReverseIteratorOperatorPlus() {
+    ft::vector<int> v;
+    for (int i = 0; i < 10; i++) v.push_back(i);
+    typedef ft::vector<int>::iterator iter_type;
+    ft::reverse_iterator<iter_type>   rev_it;
+    rev_it = v.rbegin() + 3;
+    std::cout << "The fourth element from the end is: " << *rev_it << '\n';
+}
+
 void testReverseIteratorOperatorAsterisk() {
     ft::vector<int> v;
     for (int i = 0; i < 10; i++) v.push_back(i);
@@ -133,6 +252,17 @@ int main() {
     std::cout << std::endl;
 
     std::cout << "<< REVERSE_ITERATOR >>" << std::endl;
+    testReverseIteratorOperatorMinusNonmember();
+    testReverseIteratorOperatorPlusNonmember();
+    testReverseIteratorRelationalOperators();
+    testReverseIteratorOperatorOffset();
+    testReverseIteratorOperatorPointer();
+    testReverseIteratorOperatorMinusEqual();
+    testReverseIteratorOperatorMinusMinus();
+    testReverseIteratorOperatorMinus();
+    testReverseIteratorOperatorPlusEqual();
+    testReverseIteratorOperatorPlusPlus();
+    testReverseIteratorOperatorPlus();
     testReverseIteratorOperatorAsterisk();
     testReverseIteratorBase();
     testReverseIteratorConstructor();
