66b876bfcf31de102acdbafb048d658d5906e35f
solareenlo <solareenlo@gmail.com>
2021-12-18 02:51:06 +0900

    Fix: stack.hpp

--- stack.hpp
+++ stack.hpp
@@ -6,7 +6,7 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/12 12:01:11 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/12/18 02:05:51 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/12/18 02:50:54 by tayamamo         ###   ########.fr       */
 /* ************************************************************************** */
 
 #ifndef STACK_HPP_
@@ -27,6 +27,9 @@ class stack {
     container_type m_ctnr_;
 
  public:
+    container_type get_container() const { return m_ctnr_; }
+
+ public:
     explicit stack(const container_type& ctnr = container_type())
         : m_ctnr_(ctnr) {}
 
@@ -41,35 +44,35 @@ class stack {
 template <class T, class Container>
 bool operator==(const stack<T, Container>& lhs,
                 const stack<T, Container>& rhs) {
-    return lhs.m_ctnr_ == rhs.m_ctnr_;
+    return lhs.get_container() == rhs.get_container();
 }
 
 template <class T, class Container>
 bool operator!=(const stack<T, Container>& lhs,
                 const stack<T, Container>& rhs) {
-    return !(lhs.m_ctnr_ == rhs.m_ctnr_);
+    return !(lhs.get_container() == rhs.get_container());
 }
 
 template <class T, class Container>
 bool operator<(const stack<T, Container>& lhs, const stack<T, Container>& rhs) {
-    return lhs.m_ctnr_ < rhs.m_ctnr_;
+    return lhs.get_container() < rhs.get_container();
 }
 
 template <class T, class Container>
 bool operator<=(const stack<T, Container>& lhs,
                 const stack<T, Container>& rhs) {
-    return !(rhs.m_ctnr_ < lhs.m_ctnr_);
+    return !(rhs.get_container() < lhs.get_container());
 }
 
 template <class T, class Container>
 bool operator>(const stack<T, Container>& lhs, const stack<T, Container>& rhs) {
-    return rhs.m_ctnr_ < lhs.m_ctnr_;
+    return rhs.get_container() < lhs.get_container();
 }
 
 template <class T, class Container>
 bool operator>=(const stack<T, Container>& lhs,
                 const stack<T, Container>& rhs) {
-    return !(lhs.m_ctnr_ < rhs.m_ctnr_);
+    return !(lhs.get_container() < rhs.get_container());
 }
 
 }  // namespace ft
